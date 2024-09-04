c09ffc4e272931a3260b0a4573e211c9987cd7ae
Kuuhaku <aim.tantiew@gmail.com>
2024-09-05 00:05:15 +0700

    finish add FooterSection content

--- src/components/footer/FooterSection.tsx
+++ src/components/footer/FooterSection.tsx
@@ -1,8 +1,14 @@
+"use client";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { Box, Button } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import CallIcon from "@mui/icons-material/Call";
+import PhoneIcon from "@mui/icons-material/Phone";
+import HomeIcon from "@mui/icons-material/Home";
+import { useCallback } from "react";
 
 const RecommendMenuCard = async ({ params }: any) => {
 	return (
@@ -14,10 +20,71 @@ const RecommendMenuCard = async ({ params }: any) => {
 	);
 };
 
-const FooterSection = async ({ params }: any) => {
+const FooterSection = ({ params }: any) => {
+	const videoClick = () => {
+		window.open(
+			"https://www.tiktok.com/@pantawan.farm",
+			"_blank",
+			"noopener,noreferrer"
+		);
+	};
+	const handleFacebook = useCallback(() => {
+		window.open(
+			"https://www.facebook.com/SchluckRestaurant/",
+			"_blank",
+			"noopener,noreferrer"
+		);
+	}, []);
+	const handleIG = useCallback(() => {
+		window.open(
+			"https://www.instagram.com/schluck1993/",
+			"_blank",
+			"noopener,noreferrer"
+		);
+	}, []);
 	return (
 		<>
-			<Button startIcon={<FacebookIcon />}></Button>
+			<Box>
+				<Typography variant="h5" component="h5">
+					ติดตามเราได้ที่
+				</Typography>
+				<Button startIcon={<FacebookIcon />} onClick={handleFacebook}>
+					<Typography>Schluck</Typography>
+				</Button>
+				<Button startIcon={<InstagramIcon />} onClick={handleIG}>
+					<Typography>schluck1993</Typography>
+				</Button>
+			</Box>
+			<Box>
+				<Typography variant="h5" component="h5">
+					ช่องทางการติดต่อ
+				</Typography>
+				<Box sx={{ mt: 2 }}>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ display: "flex", alignItems: "center" }}
+					>
+						<PhoneIcon sx={{ mr: 1 }} />
+						เบอร์โทร
+					</Typography>
+					<Typography variant="body1">0813559477</Typography>
+				</Box>
+				<Box sx={{ mt: 2 }}>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ display: "flex", alignItems: "center" }}
+					>
+						<HomeIcon sx={{ mr: 1 }} />
+						ที่อยู่
+					</Typography>
+					<Typography variant="body1">
+						222 ม.4 ถ.มหาดไทย ต.ท่ามะขาม อ.เมืองกาญจนบุรี
+						จ.กาญจนบุรี
+					</Typography>
+				</Box>
+			</Box>
 		</>
 	);
 };
